a12147a6e5ad2a2802a138e9f0f56d54dddab900
Jonathan Barnett <8984719+jbcbdse@users.noreply.github.com>
2025-02-16 14:04:09 -0600

    Fix config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,11 @@
 import eslint from "@eslint/js";
-import tseslint from 'typescript-eslint';
+import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   eslint.configs.recommended,
   tseslint.configs.strict,
   tseslint.configs.stylistic,
   {
-    files: ["src/**/*.ts"],
     rules: {
       "no-console": "warn",
     }
